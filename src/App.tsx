f31840c8820714402d475eacbd3e44b450c243fc
Max Bovtun <maxbovtun@gmail.com>
2022-10-28 23:25:44 +0300

    debounce

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useCallback, useState} from 'react';
 import './App.css';
+import 'bulma/css/bulma.min.css';
+
+const DEBOUNCE_WAIT = 1000;
+
+// DEBOUNCE DECORATOR:
+const debounce = (func: (qwery: string) => void, wait: number) => {
+  let timer: NodeJS.Timeout;
+
+  return (newQwery: string) => {
+    clearTimeout(timer);
+
+    timer = setTimeout(func, wait, newQwery);
+  };
+};
 
 function App() {
+  const [qwery, setQwery] = useState('');
+  const [inputQwery, setInputQwery] = useState('');
+
+  const setQweryDebounce = useCallback(
+      debounce(setQwery, DEBOUNCE_WAIT),
+      [],
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="box">
+        <h1 className="title is-2">Debounce example:</h1>
+
+        <div className="field has-addons">
+            <p className="control">
+                <input
+                    className="input"
+                    type="text"
+                    placeholder="Amount of money"
+                    value={inputQwery}
+                    onChange={(event) => {
+                        setQweryDebounce(event.target.value);
+                        setInputQwery(event.target.value);
+                    }}
+                />
+            </p>
+
+            <p className="control">
+                <button
+                    className="button is-primary"
+                    onClick={() => {
+                        setInputQwery('');
+                        setQwery('');
+                    }}
+                >
+                    Clear
+                </button>
+            </p>
+        </div>
+
+        <div className="field has-addons">
+            <p className="control title is-4">
+                {`Current value: ${qwery}`}
+            </p>
+        </div>
     </div>
   );
 }
